7f7873af9a183d738a659a6091178643d1cd5030
Delirium <daniel.castillo00@gmail.com>
2015-08-25 17:58:00 -0500

    Hotfix

--- OkMoviApp/src/com/okmm/alert/util/JsonUtil.java
+++ OkMoviApp/src/com/okmm/alert/util/JsonUtil.java
@@ -11,8 +11,12 @@ public class JsonUtil {
   public static Campaign getCampaign(JSONObject jsonObject) throws JSONException {
 	Campaign campaign = new Campaign();
 	campaign.setId(jsonObject.getInt("id_camp"));
-	campaign.setPopup(Config.IMG_BASE_PATH + jsonObject.getString("pop_up"));
-	campaign.setBackground(Config.IMG_BASE_PATH + jsonObject.getString("background"));
+	if(!jsonObject.getString("pop_up").isEmpty()){
+	  campaign.setPopup(Config.IMG_BASE_PATH + jsonObject.getString("pop_up"));
+	}
+	if(!jsonObject.getString("background").isEmpty()){
+	  campaign.setBackground(Config.IMG_BASE_PATH + jsonObject.getString("background"));
+	}
 	campaign.setLockscreen(Config.IMG_BASE_PATH + jsonObject.getString("lockscreen"));
 	campaign.setLink(jsonObject.getString("link"));
 	return campaign;
